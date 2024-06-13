07ee43dc342d7c2714e22d6b80159c22a9d0f2b4
thikhom <241295@virtualwindow.co.za>
2024-06-13 23:00:48 +0200

    Cart Modal

--- Thikho_Munyai_241295_Term1_Task3/js/scripts.js
+++ Thikho_Munyai_241295_Term1_Task3/js/scripts.js
@@ -1,102 +1,34 @@
-const minus = document.querySelector(".minus");
-const add = document.querySelector(".add");
-const value = document.querySelector(".value");
+var modal = document.getElementById("sidebar");
 
-
-let totalValue = 0;
-value.innerHTML = totalValue;
-
-add.onclick = function(){
-    totalValue++;
-    value.innerHTML = totalValue;
+function openModal(){
+    modal.style.display = "block";
 }
 
-minus.onclick = function(){
-    if (totalValue>0){
-    totalValue--;
-    value.innerHTML = totalValue;
-    } 
+function closeSidebar(){
+    modal.style.display = "none";
 }
 
-const minusNeptune = document.querySelector(".minusNeptune");
-const addNeptune = document.querySelector(".addNeptune");
-const valueNeptune = document.querySelector(".valueNeptune");
-
-valueNeptune.innerHTML = totalValue;
-
-addNeptune.onclick = function(){
-    totalValue++;
-    valueNeptune.innerHTML = totalValue;
+function add(value_text){
+    let value = parseInt(document.getElementById(value_text).value);
+    value++;
+    document.getElementById(value_text).value = value;
 }
 
-minusNeptune.onclick = function(){
-    totalValue--;
-    valueNeptune.innerHTML = totalValue;
+function minus(value_text){
+    let value = parseInt(document.getElementById(value_text).value);
+    if(value > 0){
+
+        value--;
+    }
+    document.getElementById(value_text).value = value;
 }
 
-const minusJupiter = document.querySelector(".minusJupiter");
-const addJupiter = document.querySelector(".addJupiter");
-const valueJupiter = document.querySelector(".valueJupiter");
 
-valueJupiter.innerHTML = totalValue;
 
-addJupiter.onclick = function(){
-    totalValue++;
-    valueJupiter.innerHTML = totalValue;
-}
 
-minusJupiter.onclick = function(){
-    totalValue--;
-    valueJupiter.innerHTML = totalValue;
-}
 
-const minusSaturn = document.querySelector(".minusSaturn");
-const addSaturn = document.querySelector(".addSaturn");
-const valueSaturn = document.querySelector(".valueSaturn");
 
-valueSaturn.innerHTML = totalValue;
 
-addSaturn.onclick = function(){
-    totalValue++;
-    valueSaturn.innerHTML = totalValue;
-}
-
-minusSaturn.onclick = function(){
-    totalValue--;
-    valueSaturn.innerHTML = totalValue;
-}
-
-const minusUranus = document.querySelector(".minusUranus");
-const addUranus = document.querySelector(".addUranus");
-const valueUranus = document.querySelector(".valueUranus");
-
-valueUranus.innerHTML = totalValue;
-
-addUranus.onclick = function(){
-    totalValue++;
-    valueUranus.innerHTML = totalValue;
-}
-
-minusUranus.onclick = function(){
-    totalValue--;
-    valueUranus.innerHTML = totalValue;
-}
-
-const minusMoon = document.querySelector(".minusMoon");
-const addMoon = document.querySelector(".addMoon");
-const valueMoon = document.querySelector(".valueMoon");
-
-valueMoon.innerHTML = totalValue;
-
-addMoon.onclick = function(){
-    totalValue++;
-    valueMoon.innerHTML = totalValue;
-}
-
-minusMoon.onclick = function(){
-    totalValue--;
-    valueMoon.innerHTML = totalValue;
-}
 
 function bookNow(buttoned){
     let mars = parseInt(document.getElementById(buttoned).value);
@@ -105,25 +37,3 @@ function bookNow(buttoned){
  
 }
 
-//image slides
-
-const featuredTrip = document.querySelectorAll(".featuredTrip img");
-let imgIndex = 0;
-let intervalId = null;
-
-initializeSlider()
-   
-function initializeSlider(){
-
-    featuredTrip[imgIndex].classList.add("displaySlide");
-
-}
-function showSlide(index){
-
-}
-function prevSlide(){
-
-}
-function nextSlide(){
-
-}
